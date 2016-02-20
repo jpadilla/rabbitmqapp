9d9983fecdcc24157cb056ef11a6d9a69541abb0
José Padilla <jpadilla@webapplicate.com>
2016-02-20 08:41:31 -0400

    Initial Commit

---
+++ RabbitMQ/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RabbitMQ
+//
+//  Created by José Padilla on 2/20/16.
+//  Copyright © 2016 José Padilla. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
