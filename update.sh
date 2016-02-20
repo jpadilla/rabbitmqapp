9c4ce1a9e07263003db315fa8c14f987900910a7
José Padilla <jpadilla@webapplicate.com>
2016-02-20 14:32:49 -0400

    Add update script

---
+++ update.sh
@@ -0,0 +1,128 @@
+#!/usr/bin/env bash
+
+# =========================== CHECK FORCE FLAG =================================
+if [ "$1" == "--force" ]; then
+  FORCE=true
+fi
+
+# =========================== CURRENT VERSION INFO =============================
+echo "--> Getting version numbers"
+
+CURR_VERSION=$(curl -s https://jpadilla.github.io/rabbitmq/ | grep -o '<div class="current-version">v.*' | grep -o '[0-9]*\.[0-9]*\.[0-9]*-build\.[0-9]*')
+
+CURR_RABBITMQ=$(echo $CURR_VERSION | grep -o '^[0-9]*\.[0-9]*\.[0-9]*')
+CURR_BUILD=$(echo $CURR_VERSION | grep -o '[0-9]*$')
+
+echo " -- Current RabbitMQ.app version: $CURR_BUILD"
+
+# =========================== LATEST VERSION INFO ==============================
+# Get RabbitMQ latest stable release version
+VERSION=$(curl -s https://rabbitmq.com/download.html | grep -o 'The latest release of RabbitMQ is <b>.*</b>' | grep -o '[0-9]*\.[0-9]*\.[0-9]*')
+echo "--> Current RabbitMQ version: $VERSION"
+
+# =========================== COMPARE VERSIONS =================================
+# if [ "$FORCE" != true ] && [ "$CURR_RABBITMQ" == "$VERSION" ]; then
+#   echo " -- No need to update :)"
+#   echo "==> Done!"
+#   exit 0
+# fi
+
+# =========================== DOWNLOAD =========================================
+# Create download url
+DOWNLOAD_URL="https://rabbitmq.com/releases/rabbitmq-server/v$VERSION/rabbitmq-server-generic-unix-$VERSION.tar.xz"
+
+# Download latest stable release version
+echo "--> Downloading: $DOWNLOAD_URL"
+curl -o /tmp/rabbitmq.tar.gz $DOWNLOAD_URL
+
+# Clean old rabbitmq dir
+VENDOR_DIR="$(pwd)/Vendor/rabbitmq"
+
+echo "--> Cleaning directory $VENDOR_DIR"
+rm -rf $VENDOR_DIR
+
+# Create dir
+echo "--> Creating directory $VENDOR_DIR"
+mkdir -p $VENDOR_DIR
+
+# Extract
+echo "--> Unzipping..."
+tar xvzf /tmp/rabbitmq.tar.gz -C /tmp
+
+# Move files
+echo "--> Moving files to $VENDOR_DIR"
+mv "/tmp/rabbitmq_server-$VERSION*/*" $VENDOR_DIR
+
+# Cleanup
+echo "--> Removing /tmp/rabbitmq.tar.gz"
+rm /tmp/rabbitmq.tar.gz
+
+echo "--> Removing /tmp/rabbitmq_server-$VERSION*/*"
+rm -r "/tmp/rabbitmq_server-$VERSION*/*"
+
+echo "--> Download completed!"
+
+# =========================== BUILD ============================================
+echo '--> Building'
+# Use sequential build numbers
+if [ "$FORCE" ]; then
+  NEW_BUILD=$((CURR_BUILD + 1))
+else
+  NEW_BUILD=1
+fi
+
+export RELEASE_VERSION="${VERSION}-build.${NEW_BUILD}"
+
+echo " -- Update Info.plist version ${RELEASE_VERSION}"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${RELEASE_VERSION}" RabbitMQ/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${RELEASE_VERSION}" RabbitMQ/Info.plist
+
+echo " -- Clean build folder"
+rm -rf build/
+
+echo " -- Build with defaults"
+xcodebuild
+
+echo " -- Build completed!"
+
+# =========================== RELEASE ==========================================
+echo '--> Release'
+echo " -- Zip"
+cd build/Release
+zip -r -y "/tmp/RabbitMQ.zip" RabbitMQ.app
+cd ../../
+
+# Get zip file size
+FILE_SIZE=$(du "/tmp/RabbitMQ.zip" | cut -f1)
+
+echo " -- Create AppCast post"
+rm -rf ./_posts/release
+mkdir -p ./_posts/release/
+
+echo "---
+version: $RELEASE_VERSION
+rabbitmq_version: $VERSION
+package_url: https://github.com/jpadilla/rabbitmq/releases/download/$RELEASE_VERSION/RabbitMQ.zip
+package_length: $FILE_SIZE
+category: release
+---
+- Updates RabbitMQ to $VERSION
+" > ./_posts/release/$(date +"%Y-%m-%d")-${RELEASE_VERSION}.md
+
+# =========================== PUBLISH ==========================================
+echo ""
+echo "================== Next steps =================="
+echo ""
+echo "git commit -am $RELEASE_VERSION"
+echo "git tag $RELEASE_VERSION"
+echo "git push origin --tags"
+echo ""
+echo "Upload /tmp/RabbitMQ.zip to GitHub"
+echo "https://github.com/jpadilla/rabbitmq/releases/tag/$RELEASE_VERSION"
+echo ""
+echo "git co gh-pages"
+echo "git add ."
+echo "git commit -am 'Release $RELEASE_VERSION'"
+echo "git push origin gh-pages"
+echo ""
+echo "==> Done!"
