66a0140b389c15196f86ba424c0739a1be9386d7
José Padilla <jpadilla@webapplicate.com>
2016-02-20 16:04:26 -0400

    Enable plugin on startup

--- RabbitMQ/AppDelegate.swift
+++ RabbitMQ/AppDelegate.swift
@@ -12,98 +12,117 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var paths = NSSearchPathForDirectoriesInDomains(
         NSSearchPathDirectory.DocumentDirectory,
         NSSearchPathDomainMask.UserDomainMask, true)
-    
+
     var documentsDirectory: AnyObject
     var dataPath: String
     var logPath: String
     var environment: [String: String]
-    
+
     var task: NSTask = NSTask()
     var pipe: NSPipe = NSPipe()
     var file: NSFileHandle
-    
+
     var statusBar = NSStatusBar.systemStatusBar()
     var statusBarItem: NSStatusItem = NSStatusItem()
     var menu: NSMenu = NSMenu()
-    
+
     var statusMenuItem: NSMenuItem = NSMenuItem()
-    var openCLIMenuItem: NSMenuItem = NSMenuItem()
+    var openUIMenuItem: NSMenuItem = NSMenuItem()
     var openLogsMenuItem: NSMenuItem = NSMenuItem()
     var docsMenuItem: NSMenuItem = NSMenuItem()
     var aboutMenuItem: NSMenuItem = NSMenuItem()
     var versionMenuItem: NSMenuItem = NSMenuItem()
     var quitMenuItem: NSMenuItem = NSMenuItem()
     var updatesMenuItem: NSMenuItem = NSMenuItem()
-    
+
     override init() {
         self.file = self.pipe.fileHandleForReading
         self.documentsDirectory = self.paths[0]
         self.dataPath = documentsDirectory.stringByAppendingPathComponent("RabbitMQData")
         self.logPath = documentsDirectory.stringByAppendingPathComponent("RabbitMQData/Logs")
-        
+
         self.environment = [
             "HOME": NSProcessInfo.processInfo().environment["HOME"]!,
             "RABBITMQ_MNESIA_BASE": self.dataPath,
-            "RABBITMQ_LOG_BASE": self.logPath
+            "RABBITMQ_LOG_BASE": self.logPath,
+            "RABBITMQ_ENABLED_PLUGINS_FILE": self.dataPath + "/enabled_plugins",
+            "RABBITMQ_NODENAME": "rabbit@localhost",
+            "RABBITMQ_NODE_IP_ADDRESS": "127.0.0.1"
         ]
-        
+
         super.init()
     }
-    
+
     func startServer() {
         self.task = NSTask()
         self.pipe = NSPipe()
         self.file = self.pipe.fileHandleForReading
-        
+
         if let path = NSBundle.mainBundle().pathForResource("rabbitmq-server", ofType: "", inDirectory: "Vendor/rabbitmq/sbin") {
             self.task.launchPath = path
         }
 
         self.task.environment = self.environment
         self.task.standardOutput = self.pipe
-        
+
         print("Run rabbitmq-server")
-        
+
         self.task.launch()
+
+        print("Enabling rabbitmq_management plugin")
+        self.enablePlugin("rabbitmq_management")
     }
-    
+
+    func enablePlugin(plugin: String) {
+        let task = NSTask()
+
+        if let path = NSBundle.mainBundle().pathForResource("rabbitmq-plugins", ofType: "", inDirectory: "Vendor/rabbitmq/sbin") {
+            task.launchPath = path
+        }
+
+        task.arguments = ["enable", plugin]
+        task.environment = self.environment
+
+        print("Run rabbitmq-plugins enable")
+        task.launch()
+    }
+
     func stopServer() {
         let task = NSTask()
-        
+
         if let path = NSBundle.mainBundle().pathForResource("rabbitmqctl", ofType: "", inDirectory: "Vendor/rabbitmq/sbin") {
             task.launchPath = path
         }
-        
+
         task.arguments = ["stop"]
         task.environment = self.environment
-        
+
         print("Run rabbitmqctl stop")
         task.launch()
 
-        
         let data: NSData = self.file.readDataToEndOfFile()
         self.file.closeFile()
-        
+
         let output: String = NSString(data: data, encoding: NSUTF8StringEncoding)! as String
         print(output)
     }
-    
+
     func openUI(sender: AnyObject) {
         if let url: NSURL = NSURL(string: "http://localhost:15672") {
             NSWorkspace.sharedWorkspace().openURL(url)
         }
     }
-    
+
     func openDocumentationPage(send: AnyObject) {
-        if let url: NSURL = NSURL(string: "https://github.com/jpadilla/rabbitmqapp") {
+        if let url: NSURL = NSURL(string: "https://jpadilla.github.io/rabbitmqapp/") {
             NSWorkspace.sharedWorkspace().openURL(url)
         }
     }
-    
+
     func openLogsDirectory(send: AnyObject) {
         NSWorkspace.sharedWorkspace().openFile(self.logPath)
     }
-    
+
     func createDirectories() {
         if (!NSFileManager.defaultManager().fileExistsAtPath(self.dataPath)) {
             do {
@@ -113,7 +132,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 print("Something went wrong creating dataPath")
             }
         }
-        
+
         if (!NSFileManager.defaultManager().fileExistsAtPath(self.logPath)) {
             do {
                 try NSFileManager.defaultManager()
@@ -122,76 +141,76 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 print("Something went wrong creating logPath")
             }
         }
-        
+
         print("RabbitMQ data directory: \(self.dataPath)")
         print("RabbitMQ logs directory: \(self.logPath)")
     }
-    
+
     func checkForUpdates(sender: AnyObject?) {
         print("Checking for updates")
     }
-    
+
     func setupSystemMenuItem() {
         // Add statusBarItem
         statusBarItem = statusBar.statusItemWithLength(-1)
         statusBarItem.menu = menu
-        
+
         let icon = NSImage(named: "logo")
         icon!.template = true
         icon!.size = NSSize(width: 18, height: 18)
         statusBarItem.image = icon
-        
+
         // Add version to menu
         versionMenuItem.title = "RabbitMQ"
         if let version = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as! String? {
             versionMenuItem.title = "RabbitMQ v\(version)"
         }
         menu.addItem(versionMenuItem)
-        
+
         // Add actionMenuItem to menu
-        statusMenuItem.title = "Running on Port 5672"
+        statusMenuItem.title = "Running on port 5672"
         menu.addItem(statusMenuItem)
-        
+
         // Add separator
         menu.addItem(NSMenuItem.separatorItem())
-        
-        // Add open redis-cli to menu
-        openCLIMenuItem.title = "Open Management UI"
-        openCLIMenuItem.action = Selector("openUI:")
-        menu.addItem(openCLIMenuItem)
-        
+
+        // Add open management ui to menu
+        openUIMenuItem.title = "Open Management UI"
+        openUIMenuItem.action = Selector("openUI:")
+        menu.addItem(openUIMenuItem)
+
         // Add open logs to menu
         openLogsMenuItem.title = "Open logs directory"
         openLogsMenuItem.action = Selector("openLogsDirectory:")
         menu.addItem(openLogsMenuItem)
-        
+
         // Add separator
         menu.addItem(NSMenuItem.separatorItem())
-        
+
+        // Add docs to menu
+        docsMenuItem.title = "Documentation"
+        docsMenuItem.action = Selector("openDocumentationPage:")
+        menu.addItem(docsMenuItem)
+
         // Add check for updates to menu
-        updatesMenuItem.title = "Check for Updates..."
+        updatesMenuItem.title = "Check for updates"
         updatesMenuItem.action = Selector("checkForUpdates:")
         menu.addItem(updatesMenuItem)
-        
+
         // Add about to menu
         aboutMenuItem.title = "About"
         aboutMenuItem.action = Selector("orderFrontStandardAboutPanel:")
         menu.addItem(aboutMenuItem)
-        
-        // Add docs to menu
-        docsMenuItem.title = "Documentation..."
-        docsMenuItem.action = Selector("openDocumentationPage:")
-        menu.addItem(docsMenuItem)
-        
+
         // Add separator
         menu.addItem(NSMenuItem.separatorItem())
-        
+
         // Add quitMenuItem to menu
         quitMenuItem.title = "Quit"
         quitMenuItem.action = Selector("terminate:")
         menu.addItem(quitMenuItem)
     }
-    
+
     func appExists(appName: String) -> Bool {
         let found = [
             "/Applications/\(appName).app",
@@ -206,16 +225,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                     return true;
                 }
         }
-        
+
         return found
     }
-    
+
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         createDirectories()
         setupSystemMenuItem()
         startServer()
     }
-    
+
     func applicationWillTerminate(notification: NSNotification) {
         stopServer()
     }
